c30d2c3f8a1422a10f84ae95a22970fa5169b7a0
Roman Ivaskevich <ivan432008@gmail.com>
2022-09-23 12:33:48 +0300

    add localStorage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,48 @@
 import menuCardsTpl from './templates/menu-card.hbs';
 import menu from './menu.json';
 
-const refs = {};
-
 const menuRef = document.querySelector('.js-menu');
 const menuMarkup = menuCardsTpl(menu);
 menuRef.insertAdjacentHTML('beforeend', menuMarkup);
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const THEME_KEY = 'theme';
+
+const themeRef = document.querySelector('#theme-switch-toggle');
+
+themeRef.addEventListener('change', onThemeChange);
+
+function onThemeChange(e) {
+  if (e.target.checked) {
+    replaceTheme(Theme.LIGHT, Theme.DARK);
+  } else {
+    replaceTheme(Theme.DARK, Theme.LIGHT);
+  }
+}
+
+function replaceTheme(oldTheme, newTheme) {
+  document.body.classList.replace(oldTheme, newTheme);
+
+  localStorage.setItem(THEME_KEY, newTheme);
+}
+
+function savedTheme() {
+  const savedTheme = localStorage.getItem(THEME_KEY);
+  if (savedTheme) {
+    document.body.classList.value = savedTheme;
+
+    themeRef.checked = savedTheme === Theme.DARK;
+
+    // if (savedTheme === Theme.DARK) {
+    //   themeRef.checked = true;
+    // }
+  } else {
+    document.body.classList = Theme.LIGHT;
+  }
+}
+
+savedTheme();
